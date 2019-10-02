c05e87634d62afff9ee8e3afd0b05c155efd2b58
JJC <jjc@nlplab.cc>
2019-10-02 17:30:27 +0800

    rewrite diff to parallel

--- format/diff_to_parallel.py
+++ format/diff_to_parallel.py
@@ -4,15 +4,16 @@ import fileinput
 from .diff_format import iter_edit
 
 
-def diff_to_parallel(iterable):
-    for text in map(str.strip, iterable):
-        src = list(iter_edit(text, 'delete'))
-        tgt = list(iter_edit(text, 'insert'))
-        yield src, tgt
+def diff_to_parallel(text, return_list=False):
+    if return_list:
+        return tuple(iter_edit(text, 'delete')), tuple(iter_edit(text, 'insert'))
+    else:
+        return ' '.join(iter_edit(text, 'delete')), ' '.join(iter_edit(text, 'insert'))
 
 
 def main(iterable, ignore_len=2):
-    for src, tgt in diff_to_parallel(iterable, ignore_len=0):
+    for text in map(str.strip, iterable):
+        src, tgt = diff_to_parallel(text, return_list=True)
         if len(src) > ignore_len and len(tgt) > ignore_len:
             print(' '.join(src), ' '.join(tgt), sep='\t')
 
