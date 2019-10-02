c470b8db25ab98b8f272e9ba7e40b15919464164
JJC <jjc@nlplab.cc>
2019-10-02 21:34:37 +0800

    move text strip and fileinput module import

--- format/diff_to_parallel.py
+++ format/diff_to_parallel.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-import fileinput
-
 from .diff import iter_edit
 
 
 def diff_to_parallel(text, return_list=False):
+    text = text.strip()
     if return_list:
         return tuple(iter_edit(text, 'delete')), tuple(iter_edit(text, 'insert'))
     else:
@@ -12,13 +11,14 @@ def diff_to_parallel(text, return_list=False):
 
 
 def main(iterable, ignore_len=2):
-    for text in map(str.strip, iterable):
+    for text in iterable:
         src, tgt = diff_to_parallel(text, return_list=True)
         if len(src) > ignore_len and len(tgt) > ignore_len:
             print(' '.join(src), ' '.join(tgt), sep='\t')
 
 
 if __name__ == '__main__':
+    import fileinput
     main(fileinput.input())
 
 # cat diff.txt|python diff_to_parallel.py
