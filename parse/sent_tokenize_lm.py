d3b19faff8e430608b8d22ec26bc817cd5530186
JJC <jjc@nlplab.cc>
2019-10-02 22:28:36 +0800

    organize code in sent_tokenize_lm

--- parse/sent_tokenize_lm.py
+++ parse/sent_tokenize_lm.py
@@ -5,11 +5,9 @@ import kenlm
 import string
 import os
 import re
-import fileinput
 import logging
 
 from .spacy_tokenize import word_tokenize
-from nltk import sent_tokenize as nltk_sent_tokenize
 from nltk import wordpunct_tokenize
 
 
@@ -17,20 +15,18 @@ SEP_RE = re.compile(r'((\s*[\.\!\?])+(\s*[\"\'\,])*)+')
 MASK_RE = re.compile(r'\s*\{\}\s*')
 
 
-def _nltk_sent_tokenize(text):
-    # sent tokenize using existing tokenizer first
-    sents = nltk_sent_tokenize(text)
-    # recover bad tokenization in nltk when faced with 3 or more ! and ?
-    if len(sents) > 1 and sents[-1].strip() in ('!', '?'):
-        sents[-2] += sents[-1]
-        del sents[-1]
-    return sents
+def sent_score(text, word_tokenized=True):
+    text = text if word_tokenized else capitokenize(text)
+    # remove masked tokens
+    text = MASK_RE.sub(' ', text)
+    text = ' '.join(wordpunct_tokenize(text))
+    score = lm.score(text)
+    logging.info(f'{score}: {text}')
+    return score
 
 
-def _sent_tokenize(text):
-    text = text.strip()
-    sep_indice = [m.end() for m in SEP_RE.finditer(text) if m.start() > 0 and len(m.group(0).strip()) > 1]
-    return separate_text(text, sep_indice, tokenize_word=False)
+def sents_score(sents, word_tokenized=True):
+    return sum(sent_score(sent, word_tokenized) for sent in sents)
 
 
 def pre_tokenize_normalize(text):
@@ -49,7 +45,7 @@ def pre_tokenize_normalize(text):
 
 # capitalize and tokenize
 def capitokenize(text, return_list=False):
-    text = pre_tokenize_normalize(text)
+    text = pre_tokenize_normalize(text.strip())
     tokens = word_tokenize(text.strip())
     if tokens:
         for ch in tokens[0]:
@@ -67,20 +63,6 @@ def separate_text(text, indice, tokenize_word=True):
     return list(map(capitokenize, texts) if tokenize_word else texts)
 
 
-def sent_score(text, word_tokenized=True):
-    text = text if word_tokenized else capitokenize(text)
-    # remove masked tokens
-    text = MASK_RE.sub(' ', text)
-    text = ' '.join(wordpunct_tokenize(text))
-    score = lm.score(text)
-    logging.info(f'{score}: {text}')
-    return score
-
-
-def sents_score(sents, word_tokenized=True):
-    return sum(sent_score(sent, word_tokenized) for sent in sents)
-
-
 def try_sep_all(text, tokenize_word=True):
     sep_indice = [m.end() for m in SEP_RE.finditer(text) if m.start() > 0]
     # sep_indice.remove(len(text))
@@ -91,10 +73,10 @@ def try_sep_all(text, tokenize_word=True):
 
 
 def try_sep(text, index, tokenize_word=True):
-    # f_sent, r_sent = capitokenize(text[:index]), capitokenize(text[index:])
-    sents = separate_text(text, [index], tokenize_word)
+    sents = capitokenize(text[:index]), capitokenize(text[index:])
+    # sents = separate_text(text, [index], tokenize_word)
 
-    if sents_score(sents, tokenize_word) + 1 > sent_score(text, False):
+    if sents_score(sents, tokenize_word) + 1 > sent_score(text, word_tokenized=False):
         yield sents[0]
         text = sents[-1]
         index = 0
@@ -105,6 +87,7 @@ def try_sep(text, index, tokenize_word=True):
 
 def seperate_sents(text, start=0, tokenize_word=True):
     text = text.strip()
+    # TODO: text start after striping
     m = SEP_RE.search(text, start)
     if not m or m.end() == len(text):
         yield capitokenize(text) if tokenize_word else text
@@ -116,22 +99,25 @@ def seperate_sents(text, start=0, tokenize_word=True):
             yield sent
 
 
-def sent_tokenize(text, tokenize_word=True, pre_sent_tokenize=None):
+def sent_tokenize(text, tokenize_word=True, pre_sent_tokenize=str.splitlines):
     text = text.strip()
-    sents = pre_sent_tokenize(text) if pre_sent_tokenize else [text]
+    if not text:
+        return []
+    lines = pre_sent_tokenize(text) if pre_sent_tokenize else [text]
 
-    for sent in sents:
+    for line in map(str.strip, lines):
         # aggresively tokenize sentences and validate using LM
-        for sent_candidate in seperate_sents(sent, tokenize_word=tokenize_word):
-            yield sent_candidate
+        for sent in seperate_sents(line, tokenize_word=tokenize_word):
+            yield sent
 
 
 lm = kenlm.Model(os.environ.get('KENLM_MODEL', 'bnc.punct.bin'))
 
 
 if __name__ == "__main__":
+    import fileinput
     logging.basicConfig(level=logging.INFO)
 
-    for line in map(str.strip, fileinput.input()):
+    for line in fileinput.input():
         for sent in sent_tokenize(line):
             print(sent)
