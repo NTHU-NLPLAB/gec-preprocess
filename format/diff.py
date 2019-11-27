f78de6f2726ed4adce95ff35e079ec4dd17329b0
JJC <jjc@nlplab.cc>
2019-11-27 12:46:08 +0800

    use split and join to remove consective spaces and rewrite 'diff.py'

--- format/diff.py
+++ format/diff.py
@@ -5,34 +5,34 @@ import re
 EDIT_RE = re.compile(r'(\[-(?P<delete>[^ ]*?)-\]|\{\+(?P<insert>[^ ]*?)\+\})+(\((?P<type>[A-Za-z,]+)\))?')
 
 
-def gen_diff_token(delete, insert, err_type='', space_escape='\u3000'):
+def gen_diff_token(delete, insert, err_type=None, space_escape='\u3000'):
     diff_token = ''
     if delete:
-        diff_token += f"[-{delete.replace(' ', space_escape)}-]"
+        diff_token += f"[-{space_escape.join(delete.split())}-]"
     if insert:
-        diff_token += f"{{+{insert.replace(' ', space_escape)}+}}"
+        diff_token += f"{{+{space_escape.join(insert.split())}+}}"
     if diff_token and err_type:
-        diff_token += f"({err_type.replace(' ', space_escape)})"
+        diff_token += f"({space_escape.join(err_type.split())})"
     return diff_token
 
 
-def parse_diff_token(edit_token, space_escape='\u3000'):
+def parse_diff_token(edit_token):
     match = EDIT_RE.match(edit_token)
     assert match, f'"{edit_token}" is not a valid edit token'
-    delete, insert, err_type = match.group('delete') or '', match.group('insert') or '', match.group('type') or ''
-    delete = delete.replace(space_escape, ' ')
-    insert = insert.replace(space_escape, ' ')
-    err_type = err_type.replace(space_escape, ' ')
+    delete, insert, err_type = match.group('delete'), match.group('insert'), match.group('type')
+    delete = ' '.join(delete.split()) if delete else ''
+    insert = ' '.join(insert.split()) if insert else ''
+    err_type = ' '.join(err_type.split()) if err_type else ''
     return delete, insert, err_type
 
 
-def iter_edit(text, iter_type, skip_none=True):
+def iter_edit(text, iter_type, skip_empty=True):
     index = ('delete', 'insert', 'error').index(iter_type)
     for token in filter(None, text.split(' ')):
         if token.startswith('{+') or token.startswith('[-'):
             items = parse_diff_token(token)
         else:
-            items = (token, token, '')
+            items = (token, token, None)
 
-        if not skip_none or items[index]:
+        if not skip_empty or items[index]:
             yield items[index]
