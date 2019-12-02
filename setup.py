352f93eaa4c99a6f3a7530464cb6fcf5459797de
JJC <jjc@nlplab.cc>
2019-12-02 18:43:14 +0800

    add package install file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+
+setup(
+    name="gec-preprocess",
+    version="0.0.1",
+    author="JJC",
+    author_email="jjc@nlplab.cc",
+    description="A toolkit for preprocessing corpus for grammatical error correction.",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    url="https://github.com/NTHU-NLPLAB/gec-preprocess",
+    packages=["gecpt"],
+    install_requires=['beautifulsoup4'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    extras_require={
+        'parse':  ["spacy", "RXP"],
+    },
+    # zip_safe=False,
+    python_requires='>=3.6',
+)
