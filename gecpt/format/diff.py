0ccf53f841cc1de375768ca2d84982e6b425b7b7
JJC <jjc@nlplab.cc>
2020-02-17 13:06:24 +0800

    refactor: rewrite diff module

--- gecpt/format/diff.py
+++ gecpt/format/diff.py
@@ -2,22 +2,11 @@
 import re
 
 
-EDIT_RE = re.compile(r'(\[-(?P<delete>[^ ]*?)-\]|\{\+(?P<insert>[^ ]*?)\+\})+(\((?P<type>[^)]+)\))?')
-
-
-def gen_diff_token(delete, insert, err_type=None, space_escape='\u3000'):
-    diff_token = ''
-    if delete:
-        diff_token += f"[-{space_escape.join(delete.split())}-]"
-    if insert:
-        diff_token += f"{{+{space_escape.join(insert.split())}+}}"
-    if diff_token and err_type:
-        diff_token += f"({space_escape.join(err_type.split())})"
-    return diff_token
+DIFF_EDIT_RE = re.compile(r'(\[-(?P<delete>[^ ]*?)-\]|\{\+(?P<insert>[^ ]*?)\+\})+(\((?P<type>[^)]+)\))?')
 
 
 def parse_diff_token(edit_token):
-    match = EDIT_RE.match(edit_token)
+    match = DIFF_EDIT_RE.match(edit_token)
     assert match, f'"{edit_token}" is not a valid edit token'
     delete, insert, err_type = match.group('delete'), match.group('insert'), match.group('type')
     delete = ' '.join(delete.split()) if delete else ''
@@ -26,16 +15,42 @@ def parse_diff_token(edit_token):
     return delete, insert, err_type
 
 
-def iter_edit(iterable, iter_type, skip_empty=True):
+def _gen_diff_iter(delete, insert, err_type=None, space_escape='\u3000'):
+    if delete:
+        yield f"[-{space_escape.join(delete.split())}-]"
+    if insert:
+        yield f"{{+{space_escape.join(insert.split())}+}}"
+    if delete and insert and err_type:
+        yield f"({space_escape.join(err_type.split())})"
+
+
+def gen_diff_token(delete, insert, err_type=None, space_escape='\u3000'):
+    return ''.join(_gen_diff_iter(delete, insert, err_type, space_escape))
+
+
+def _iter_diff(iterable, iter_type):
     index = ('delete', 'insert', 'error').index(iter_type)
     # split into tokens
     tokens = iterable.split(' ') if type(iterable) is str else iterable
     # skip empty tokens
     for token in filter(None, tokens):
         if token.startswith(('{+', '[-')):
-            items = parse_diff_token(token)
+            yield parse_diff_token(token)[index]
         else:
-            items = (token, token, None)
+            yield token if index < 2 else None
 
-        if not skip_empty or items[index]:
-            yield items[index]
+
+def iter_diff(iterable, iter_type, skip_empty=True):
+    if skip_empty:
+        return filter(None, _iter_diff(iterable, iter_type))
+    else:
+        return _iter_diff(iterable, iter_type)
+
+
+def diff_to_parallel(text, return_list=False):
+    text = text.strip()
+    before, after = iter_diff(text, 'delete'), iter_diff(text, 'insert')
+    if return_list:
+        return tuple(before), tuple(after)
+    else:
+        return ' '.join(before), ' '.join(after)
